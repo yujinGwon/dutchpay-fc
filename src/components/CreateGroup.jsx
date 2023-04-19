85e2ac2544693666b22baffe91e63df9013e05b3
yujinGwon <gwonyj36@gmail.com>
2023-04-19 14:45:11 +0900

    Styling 컴포넌트

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -3,57 +3,82 @@ import { CenteredOverlayForm } from './CenteredOverlayForm';
 import { useRecoilState, useResetRecoilState } from 'recoil';
 import { groupNameState } from '../state/groupName';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 export const CreateGroup = () => {
   const [validated, setValidated] = useState(false);
   const [validGroupName, setValidGroupName] = useState(false);
   const [groupName, setGroupName] = useRecoilState(groupNameState);
-  
+
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const form = event.currentTarget;
     if (form.checkValidity()) {
       setValidGroupName(true)
     } else {
-      event.stopPropagation();
+      event.stopPropagation()
       setValidGroupName(false)
     }
     setValidated(true)
-  };
+  }
 
   return (
-    <div>
-      <h1>Dutch Pay</h1>
+      <CenteredOverlayForm>
+        <Container>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <StyledRow>
+            <Row className='align-items-start'>
+              <StyledH2>먼저, 더치 페이 할 그룹의 이름을 정해볼까요?</StyledH2>
+            </Row>
+            <Row className='align-items-center'>
+              <Form.Group controlId="validationGroupName">
+                <Form.Control
+                  type="text"
+                  required
+                  placeholder="2022 제주도 여행"
+                  onChange={(e) => setGroupName(e.target.value)}
+                />
+                <Form.Control.Feedback
+                  type="invalid"
+                  data-valid={validGroupName}
+                >
+                  그룹 이름을 입력해 주세요.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className='align-items-end'>
+              <StyledSubmitButton>저장</StyledSubmitButton>
+            </Row>
+            </StyledRow>
+          </Form>
+        </Container>
+      </CenteredOverlayForm>
+  )
+}
 
-      <Container>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row>
-            <h2>먼저, 더치 페이 할 그룹의 이름을 정해볼까요?</h2>
-          </Row>
-          <Row>
-            <Form.Group controlId="validationGroupName">
-              <Form.Control
-                type="text"
-                required
-                placeholder="2022 제주도 여행"
-                onChange={(e) => setGroupName(e.target.value)}
-              />
-              <Form.Control.Feedback 
-                type="invalid"
-                data-valid={validGroupName}
-              >
-                그룹 이름을 입력해 주세요.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Row>
-            <Button type="submit">저장</Button>
-          </Row>
-        </Form>
-      </Container>
+const StyledH2 = styled.h2`
+  font-weight: 700;
+  line-height: 35px;
+  test-align: right;
+  overflow-wra: break-word;
+  word-break: keep-all;
+`
 
-      {/* <CenteredOverlayForm />     */}
-    </div>
-  );
-};
+const StyledSubmitButton = styled(Button).attrs({
+  type: 'submit'
+})`
+  background-color: #6610F2;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: #6610F2;
+    filter: brightness(80%);
+  }
+`
+
+const StyledRow = styled(Row)`
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+`
